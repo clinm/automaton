b990424896e787edb731e6f64a33325435b2c160
Matthieu <Matthieu.clin@wanadoo.fr>
2015-12-29 12:11:14 +0100

    Use dataFormBinding library

--- websrc/script-index.js
+++ websrc/script-index.js
@@ -12,30 +12,7 @@ var autoParams = {  surfaceId: 'canvas-automaton',
 
 var auto = Automaton.automata(autoParams);
 
-/**
- * Each element of the option panel gets a listener and we
- * put the value declared in autoParams in the inputs
- *
- */
-
 var autoType = document.getElementById('automaton-type');
-autoType.addEventListener('change', onAutomatonUpdate ,false);
-
-var autoFreq = document.getElementById('automaton-frequency');
-autoFreq.value = autoParams.freqUpdate;
-autoFreq.addEventListener('change', onUpdateFrequency, false);
-
-var autoWidth = document.getElementById('automaton-width');
-autoWidth.value = autoParams.numberElements[0];
-autoWidth.addEventListener('change', onUpdateWidth, false);
-
-var autoHeight = document.getElementById('automaton-height');
-autoHeight.value = autoParams.numberElements[1];
-autoHeight.addEventListener('change', onUpdateHeight, false);
-
-var autoCircular = document.getElementById('automaton-circular');
-autoCircular.checked = autoParams.circular;
-autoCircular.addEventListener('change', onUpdateCircular, false);
 
 var automatonStartStop = document.getElementById('automaton-startStop');
 var running = false;
@@ -46,30 +23,6 @@ Object.keys(Automaton.lifeAutomatons).forEach(function(a){
     opt.innerHTML = Automaton.lifeAutomatons[a].name;
     autoType.appendChild(opt);
 });
-
-/**
- * Several functions to update values
- */
-
-function onAutomatonUpdate(){
-    autoParams.auto =  autoType.options[autoType.selectedIndex].value;
-}
-
-function onUpdateFrequency(){
-    autoParams.freqUpdate = autoFreq.value;
-}
-
-function onUpdateWidth(){
-    autoParams.numberElements[0] = autoWidth.value;
-}
-
-function onUpdateHeight(){
-    autoParams.numberElements[1] = autoHeight.value;
-}
-
-function onUpdateCircular(){
-    autoParams.circular = autoCircular.checked;
-}
 
 function newAuto(){
     auto.stop();
